4625458e7db03577d70aa914276ee2211b486dbf
bruh092 <79010262+bruh092@users.noreply.github.com>
2021-05-27 17:41:22 +0700

    a

---
+++ a.java
@@ -0,0 +1,10 @@
+public class a {
+    public static void main(String[]args){
+        String name = "Never give up";
+
+        //boolean result = name.equals("Never give up");
+        char result = name.charAt(0);
+        System.out.println(result);
+    }
+    
+}
